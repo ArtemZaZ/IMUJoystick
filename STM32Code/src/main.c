d7410183bce1880f63e2161da3fe33cc863c11c4
ArtemZaZ <temka.911@mail.ru>
2018-05-16 10:43:41 +0300

    Исправил отступы в main

--- STM32Code/src/main.c
+++ STM32Code/src/main.c
@@ -14,68 +14,68 @@ Timer actionsTimer;
 
 
 static enum {
-	FSM_STOP,	// Джойстик не работает - начальное состояние конечной машины
-	FSM_REINIT, // Переинициализация Джойстика
-	FSM_START, 	// Джойстик ничинает работать - нормальный режим
-	FSM_WAIT,	// Ожидание 
-	FSM_GET_IMU_DATA,	// Чтение данных с I2C и фильтрация
-	FSM_UPDATE, // если нужно что-то обновить
-	FSM_FILTRATION_BUTTON, // Фильтрация кнопок 
-	FSM_READ_UART, // Чтение данных с UART
-	FSM_ACTION,	// Сделать какое-нибудь действие
-	FSM_WRITE_UART	// Отправка по UART
-}	State;
+  FSM_STOP,	// Джойстик не работает - начальное состояние конечной машины
+  FSM_REINIT, // Переинициализация Джойстика
+  FSM_START, 	// Джойстик ничинает работать - нормальный режим
+  FSM_WAIT,	// Ожидание 
+  FSM_GET_IMU_DATA,	// Чтение данных с I2C и фильтрация
+  FSM_UPDATE, // если нужно что-то обновить
+  FSM_FILTRATION_BUTTON, // Фильтрация кнопок 
+  FSM_READ_UART, // Чтение данных с UART
+  FSM_ACTION,	// Сделать какое-нибудь действие
+  FSM_WRITE_UART	// Отправка по UART
+} State;
 
 
 void FSM(void)
 {
-	switch(State)
-	{
-		case FSM_STOP:
+  switch(State)
+  {
+    case FSM_STOP:
       setFlag_jWF(0); // ставим флаг, что джойстик не работает
-			State = FSM_READ_UART;	// Пока не придет сигнал START с UART	переходим к чтению
-			return;
+      State = FSM_READ_UART;	// Пока не придет сигнал START с UART	переходим к чтению
+      return;
 		
-		case FSM_REINIT:      
+    case FSM_REINIT:      
       reInitAll(); 
       resetTimer(&IMUTimer);
       resetTimer(&actionsTimer);
       getDeltaTime(&IMUTimer);
       getDeltaTime(&actionsTimer);
-			State = FSM_START;
-			return;
-			
-		case FSM_START:
+      State = FSM_START;
+      return;
+
+    case FSM_START:
       sendSTART();  // отправляем сигнал, что джойстик начал работу
       setFlag_jWF(1); // ставим флаг, что джойстик работает
-			State = FSM_GET_IMU_DATA;
-			return;
-			
-		case FSM_WAIT:
-			for(uint32_t i = 0; i < WAIT_TICKS; i++);
-			State = FSM_GET_IMU_DATA; 
-			return;
-		
-		case FSM_GET_IMU_DATA:      
-			updateIMUData(getDeltaTime(&IMUTimer));
+      State = FSM_GET_IMU_DATA;
+      return;
+
+    case FSM_WAIT:
+      for(uint32_t i = 0; i < WAIT_TICKS; i++);
+      State = FSM_GET_IMU_DATA; 
+      return;
+
+    case FSM_GET_IMU_DATA:      
+      updateIMUData(getDeltaTime(&IMUTimer));
       if(isActiveFlag_tTSIMUDF())  // если активен флаг, что пора отправлять данные с IMU
       {
         sendIMUData();  // отправляем данные с IMU датчика
       }
-			State = FSM_FILTRATION_BUTTON;
-			return;
-		
-		case FSM_UPDATE:
-			updateActions(getDeltaTime(&actionsTimer));   
-			State = FSM_WRITE_UART;
-			return;
+      State = FSM_FILTRATION_BUTTON;
+      return;
+		  
+    case FSM_UPDATE:
+      updateActions(getDeltaTime(&actionsTimer));   
+      State = FSM_WRITE_UART;
+      return;
 			
-		case FSM_FILTRATION_BUTTON:
-			filtrationAndSendChangedButtons();
-			State = FSM_UPDATE;
-			return;
+    case FSM_FILTRATION_BUTTON:
+      filtrationAndSendChangedButtons();
+      State = FSM_UPDATE;
+      return;
 			
-		case FSM_READ_UART:
+    case FSM_READ_UART:
       if(isActiveFlag_bRC())  // если стоит флаг того, что можно читать сообщения
       {
         readMessages();
@@ -94,27 +94,27 @@ void FSM(void)
         State = FSM_STOP; 
       }
       else if(isActiveFlag_rACTIONF())	State = FSM_ACTION;	// Если пришел сигнал действия
-			else  State = FSM_WAIT; // Если ничего из этого
-			return;
+      else  State = FSM_WAIT; // Если ничего из этого
+      return;
 		
-		case FSM_ACTION:
-			act();
-			State = FSM_WAIT;
-			return;
+    case FSM_ACTION:
+      act();
+      State = FSM_WAIT;
+      return;
 		
-		case FSM_WRITE_UART:
-			BTransmit(); 
-			State = FSM_READ_UART;
-			return;
-	}		
+    case FSM_WRITE_UART:
+      BTransmit(); 
+      State = FSM_READ_UART;
+      return;
+  }		
 }
 
 
 int main(void)       		   
 {  
   InitializeAll();
-	while(1)
-	{ 
+  while(1)
+  { 
     FSM();
-	}
+  }
 }
